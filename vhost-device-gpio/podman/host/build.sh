61952258cddf5c96af77d6f805e821929c925060
Wojciech M. Zabolotny <wzab01@gmail.com>
2024-02-06 18:25:17 +0100

    Uncommented cloning the repository.

--- vhost-device-gpio/podman/host/build.sh
+++ vhost-device-gpio/podman/host/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#git clone https://github.com/wzab/vhost-device.git -b gpio-python
+git clone https://github.com/wzab/vhost-device.git -b gpio-python
 export PATH_TO_LIBGPIOD=/home/dev/libgpiod-2.1
 export SYSTEM_DEPS_LIBGPIOD_NO_PKG_CONFIG=1
 export SYSTEM_DEPS_LIBGPIOD_SEARCH_NATIVE="${PATH_TO_LIBGPIOD}/lib/.libs/"
